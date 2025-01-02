664b28f9631399005cff41c542bacf10be8b8dbb
Agustin Ancona <anconaa54@gmail.com>
2025-01-02 06:27:16 -0300

    Vista de "Deposito" finalizada.

---
+++ src/components/WarehouseComponents/Warehouse.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Title from "../GeneralComponents/Title/Title";
+import Image from "next/image";
+import warehouseImg from "@/../public/assets/images/Ilustrations/Warehouse/ilustration-warehouse.svg";
+
+const Warehouse = () => {
+  return (
+    <main>
+      <Title label="Depósito" />
+
+      <div className="lg:flex lg:items-center lg:justify-center">
+        <div className="w-full mt-6 lg:order-2">
+          <Image
+            src={warehouseImg}
+            width={500}
+            height={500}
+            alt="Imagen de Depósito"
+            className="w-[200px] mx-auto sm:w-[300px] md:w-[350px] lg:w-[430px] xl:w-[490px]"
+          />
+        </div>
+        <div
+          className="
+        text-sc text-[14px] sm:text-[16px] md:text-[20px] lg:order-1 xl:text-[24px]"
+        >
+          <h4 className="font-bold mt-6 sm:mt-[86px]">
+            Almacenamos tus paquetes en nuestro deposito, organizamos tus pedidos por zona y
+            entrgamos.
+          </h4>
+          <p className="font-medium mt-4 sm:mt-[24px]">
+            El deposito cuenta con seguridad privada 24hs.
+          </p>
+
+          <div className="mt-4 sm:mt-[24px]">
+            <p>La ubicación es 100% accesible:</p>
+            <ul className="list-disc pl-5">
+              <li>Sobre Av. Juan B. Justo</li>
+              <li>a 5 cuadras de Av. Gral Paz</li>
+              <li>a 5 cuadras de Av. Rivadavia</li>
+              <li>a 5 cuadras de Au. 25 de Mayo/Acceso Oeste</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Warehouse;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,15 +19,16 @@ export default {
         mapColorCABA: "#2EE47A",
         mapColorGBA2: "#1C9E53",
         mapColorGBA3: "#037B35",
+        blackPage: "#222",
       },
       fontFamily: {
         nunito: ["Nunito", "sans-serif"],
         bebasNeue: ["BebasNeue", "sans-serif"],
       },
       backgroundImage: {
-        buttonGradient: 'linear-gradient(350deg, #24BA63 45.45%, #2EAA62 46.73%)',
-        buttonHoverGradient: 'linear-gradient(350deg, #FAFAFA 45.45%, #EEEEEE 46.73%)',
-      }
+        buttonGradient: "linear-gradient(350deg, #24BA63 45.45%, #2EAA62 46.73%)",
+        buttonHoverGradient: "linear-gradient(350deg, #FAFAFA 45.45%, #EEEEEE 46.73%)",
+      },
       boxShadow: {
         methodCard: "0 4px 4px 0 rgba(36, 186, 99, 0.50)",
       },

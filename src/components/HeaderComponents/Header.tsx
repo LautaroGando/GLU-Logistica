707f9315561514fdfb1ad74a45b5014170fdc8ba
Lautaro Gando <lautarogandodev@gmail.com>
2025-01-03 06:34:13 -0300

    Solucionando error del size

--- src/components/HeaderComponents/Header.tsx
+++ src/components/HeaderComponents/Header.tsx
@@ -2,27 +2,21 @@
 import React from "react";
 import Logo from "./Logo/Logo";
 import ResponsiveMenu from "./ResponsiveMenu/ResponsiveMenu";
-import { useSize } from "@/hooks/useSize";
 import Button from "./Button/Button";
 import Links from "./Links/Links";
 import Banner from "./Banner/Banner";
 
 export const Header: React.FC = () => {
-  const { size } = useSize();
-
   return (
     <>
       <div className="w-full h-[50px] flex justify-center shadow-sm sticky top-0 z-50 bg-pcSecondary shadow-tcExtra sm:h-[62px] md:h-[89px]">
         <div className="w-full max-w-[1200px] h-full flex justify-between items-center px-3 sm:px-4 xl:w-[1200px] xl:p-0">
           <Logo />
-          {size && size < 1024 ? (
-            <ResponsiveMenu />
-          ) : (
-            <div className="flex items-center gap-3">
-              <Links />
-              <Button />
-            </div>
-          )}
+          <ResponsiveMenu />
+          <div className="hidden items-center gap-3 lg:flex">
+            <Links />
+            <Button />
+          </div>
         </div>
       </div>
       <Banner />

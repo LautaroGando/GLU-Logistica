8cc23dda15b8e2dcf722eb6a98d139cf1b6b972d
Lautaro Gando <lautarogandodev@gmail.com>
2025-04-06 17:56:24 -0300

    Guardando cambios.

--- src/components/HeaderComponents/Logo/Logo.tsx
+++ src/components/HeaderComponents/Logo/Logo.tsx
@@ -1,25 +1,26 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { ILogoProps } from "./types";
 
-export const Logo: React.FC = () => {
+export const Logo: React.FC<ILogoProps> = ({ lightMode }: ILogoProps) => {
   return (
- 
-    <Link href="/" className="flex gap-2 items-center transition-all duration-200 active:scale-[.9]">
+    <Link
+      href="/"
+      className="flex gap-2 items-center transition-all duration-200 active:scale-[.9]"
+    >
       <Image
-        className="w-[32px] h-[32px] sm:w-[50px] sm:h-[50px] md:w-[56px] md:h-[56px]"
-        src="/assets/images/Header/logo.svg"
+        className="w-[80px] sm:w-[120px] md:w-[140px]"
+        src={
+          lightMode
+            ? "/assets/images/Header/logoWhite.svg"
+            : "/assets/images/Header/logo.svg"
+        }
         alt="Logo del header"
-        width={56}
-        height={56}
+        width={150}
+        height={150}
         priority
       />
-      <div className="text-pcPrincipal">
-        <h3 className="text-2xl font-bebasNeue sm:text-3xl md:text-[34px]">LA VUELTA</h3>
-        <h4 className="text-[7px] tracking-[4.6px] sm:text-[10px] sm:tracking-[4.95px] md:text-xs md:tracking-[5.3px]">
-          LOGÍSTICA
-        </h4>
-      </div>
     </Link>
   );
 };

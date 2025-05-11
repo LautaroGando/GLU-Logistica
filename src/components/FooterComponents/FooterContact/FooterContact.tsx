1a0a87753df65d515438a7a794aec8105638542b
Lautaro Gando <lautarogandodev@gmail.com>
2025-05-11 01:23:22 -0300

    feat: update newsletter form submission handling and button text

--- src/components/FooterComponents/FooterContact/FooterContact.tsx
+++ src/components/FooterComponents/FooterContact/FooterContact.tsx
@@ -5,12 +5,16 @@ import { IFooterContactProps } from "./types";
 import useNewsLetter from "@/hooks/useNewsletter";
 
 const FooterContact: React.FC<IFooterContactProps> = ({ isMobile }) => {
-  const { email, setEmail, handleSubmit } = useNewsLetter();
+  const { email, setEmail } = useNewsLetter();
 
   return (
-    <div className={`text-[14px] ${isMobile ? "mt-6 sm:mt-0 lg:hidden" : "hidden lg:block"}`}>
-      <h4 className="text-[16px] font-bold sm:text-[20px] lg:text-[24px]">Estar Conectados</h4>
-      <form onSubmit={handleSubmit}>
+    <div
+      className={`text-[14px] ${isMobile ? "mt-6 sm:mt-0 lg:hidden" : "hidden lg:block"}`}
+    >
+      <h4 className="text-[16px] font-bold sm:text-[20px] lg:text-[24px]">
+        Estar Conectados
+      </h4>
+      <form onSubmit={() => {}}>
         <div>
           <input
             type="email"
@@ -24,7 +28,7 @@ const FooterContact: React.FC<IFooterContactProps> = ({ isMobile }) => {
           />
           <input
             type="submit"
-            value="Suscribirse"
+            value="Proximamente..."
             className="bg-sc p-1 transition-all duration-200 border-2 border-transparent cursor-pointer hover:bg-tcSecondary sm:text-[16px] sm:p-2"
           />
         </div>
